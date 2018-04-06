a5d0f8050c383a6f35fee11ff2caafcf0186580d
ONLYA <36487580+ONLYA@users.noreply.github.com>
2018-04-06 01:31:28 +0100

    Update sensor.h

--- MotionSensor/sensor.h
+++ MotionSensor/sensor.h
@@ -1,11 +1,13 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+#include "inv_mpu_lib/inv_mpu.h"
+
 int ms_open();
 int ms_update();
 int ms_close();
 
-#define get_temp(*data) mpu_get_temperature(*data)
+#define get_temp mpu_get_temperature
 
 uint8_t GetGravity(VectorFloat *v, Quaternion *q);
 uint8_t GetYawPitchRoll(float *data, Quaternion *q, VectorFloat *gravity); 
