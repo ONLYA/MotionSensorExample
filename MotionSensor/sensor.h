f0b9d2cea4e2fdc3aa36ff3f35b068395e61ddc5
ONLYA <36487580+ONLYA@users.noreply.github.com>
2018-04-06 01:14:49 +0100

    add temperature for convenience

--- MotionSensor/sensor.h
+++ MotionSensor/sensor.h
@@ -5,6 +5,8 @@ int ms_open();
 int ms_update();
 int ms_close();
 
+#define get_temp mpu_get_temperature
+
 uint8_t GetGravity(VectorFloat *v, Quaternion *q);
 uint8_t GetYawPitchRoll(float *data, Quaternion *q, VectorFloat *gravity); 
 uint8_t GetGyro(int32_t *data, const uint8_t* packet);
